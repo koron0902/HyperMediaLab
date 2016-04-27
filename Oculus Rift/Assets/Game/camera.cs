517817a10b434e1d184a7b41088f0eb23a3e3079
関口 恵太 <koron0902@gmail.com>
2016-04-27 15:23:06 +0900

    視点変更の導入・改良

--- Oculus Rift/Assets/Game/camera.cs
+++ Oculus Rift/Assets/Game/camera.cs
@@ -12,6 +12,8 @@ public class camera : MonoBehaviour
     public Vector3 position;
     public Quaternion rotation;
 
+    public int Camera_Radius;
+
 	void start()
 	{
         transform.position = Vector3.zero;
@@ -22,8 +24,20 @@ public class camera : MonoBehaviour
 	{
         rotation = UnityEngine.VR.InputTracking.GetLocalRotation(UnityEngine.VR.VRNode.LeftEye);
         position = UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.Head);
-        
+
         //transform.rotation = rotation;
-        //transform.position = position + new Vector3(0f, +2f, 0f);
+        Quaternion tmp = rotation;
+        if (rotation.y > 180)
+            tmp.y = rotation.y - 180;
+
+        tmp.y /= 90;
+
+        int Mul = (rotation.y > 0 ? 1 : 0);
+        transform.rotation = rotation;
+        transform.position = position + new Vector3(
+                                            (float)(-Camera_Radius * Math.Sin(rotation.y * Math.PI)) / 2,
+                                            (float)(Camera_Radius / 2 * Math.Sin(rotation.x * Math.PI)),
+                                            (float)Math.Abs((Camera_Radius * Math.Sin(rotation.y * Math.PI))) / 2 + (float)Math.Abs((Camera_Radius * Math.Sin(rotation.x * Math.PI))) / 2
+                                            );
     }
 }
